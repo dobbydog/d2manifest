c822fce3ebc5028c25945b52c9439c28a2278a6d
Shuhei Takahashi <shuhei-takahashi@und-meer.com>
2019-12-06 16:22:35 +0900

    deploy

---
+++ precache-manifest.0156c171686ba680f790cb495177b05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8722f68c1c556866ed71",
+    "url": "/d2manifest/css/app.df11975f.css"
+  },
+  {
+    "revision": "639f68da7ded87b9cb48ca337db540da",
+    "url": "/d2manifest/index.html"
+  },
+  {
+    "revision": "8722f68c1c556866ed71",
+    "url": "/d2manifest/js/app.92bf7fde.js"
+  },
+  {
+    "revision": "9cb560ea898c163cd685",
+    "url": "/d2manifest/js/chunk-vendors.e5b595bb.js"
+  },
+  {
+    "revision": "6c58be66a2a4c569056cc6ec27c28787",
+    "url": "/d2manifest/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/d2manifest/robots.txt"
+  }
+]);
